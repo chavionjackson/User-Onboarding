52654acebe2ac388fde606fb9701db61c0cc2140
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-05-12 15:29:31 -0700

    Still tweaking

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -4,13 +4,13 @@ describe('Onboard Testing', () => {
     })
 
     // it represents the test itself
-   it('sanity check', () => {
+   it('assertion', () => {
     // expect is an assertion
     // we can have many assertions in a test
     expect(2 + 2).to.equal(4)
     expect(2 + 2).not.to.equal(5)
    }) 
-
+    //GRABBED ALL INPUTS
     const textInput = () => cy.get('[name="name"]')
     const emailInput = () => cy.get('[name="email"]')
     const passwordInput = () => cy.get('[name="password"]')
@@ -18,26 +18,29 @@ describe('Onboard Testing', () => {
     const submitBtn = () => cy.get('button')
 
     it('Allows submission after completion', () => {
-        const quote = 'Satoru gojo'
-        textInput().type(quote).should('have.value', 'Satoru gojo');
-        
+        //Get the Name input and type a name in it.
+        //Use an assertion to check if the text inputted contains the name you provided
+        const quote = 'CJ'
+        textInput().type(quote).should('have.value', 'CJ');
+        // Get the Email input and type an email address in it
         const emailquote = 'chavionjackson@yahoo.com'
         emailInput().type(emailquote)
-    
+        // Get the password input and type a password in it
         const passwordquote = 'topsecret'
         passwordInput().type(passwordquote)
-    
+        // Set up a test that will check to see if a user can check the terms of service box
         termsBox().check()
-    
+        // Check to see if a user can submit the form data
         submitBtn().should('not.be.disabled')
         submitBtn().click()
   
 })
 it("will show errors for invalid inputs", () => {
-    textInput().type("p");
+    // Check for form validation if an input is left empty
+    textInput().type("Incorrect");
     submitBtn().should("be.disabled");
 
-    const emailquote = 'Sator'
+    const emailquote = 'Must be an email'
     emailInput().type(emailquote)
 
 })
